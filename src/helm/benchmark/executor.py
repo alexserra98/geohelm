85072c6e01cbffd712c0ddb166b65bd17448a6ce
alexserra98 <alex.serra.pietro@gmail.com>
2023-10-30 17:12:21 +0100

    removed_client_interface

--- src/helm/benchmark/executor.py
+++ src/helm/benchmark/executor.py
@@ -11,7 +11,38 @@ from helm.proxy.services.service import Service
 from helm.benchmark.adaptation.scenario_state import ScenarioState
 from helm.benchmark.adaptation.request_state import RequestState
 from functools import partial
+from copy import deepcopy
+import torch
+from dataclasses import asdict
+from transformers import AutoModelForCausalLM, AutoTokenizer, GenerationConfig
+from typing import Any, Dict, List
 
+from helm.common.cache import Cache, CacheConfig
+from helm.common.hierarchical_logger import htrack_block, hlog
+from helm.common.request import EMBEDDING_UNAVAILABLE_REQUEST_RESULT, Request, RequestResult, Sequence, Token
+from helm.common.tokenization_request import (
+    TokenizationRequest,
+    TokenizationRequestResult,
+    DecodeRequest,
+    DecodeRequestResult,
+    TokenizationToken,
+)
+from helm.proxy.clients.client import Client, wrap_request_time, truncate_sequence, cleanup_tokens
+from helm.proxy.clients.huggingface_tokenizer import HuggingFaceTokenizers
+from helm.proxy.clients.huggingface_model_registry import (
+    get_huggingface_model_config,
+    HuggingFaceModelConfig,
+    HuggingFaceHubModelConfig,
+    HuggingFaceLocalModelConfig,
+)
+from threading import Lock
+
+from helm.benchmark.hidden_geometry.utils import hidden_states_process
+
+from einops import reduce
+import numpy as np
+import copy
+from tqdm import tqdm
 
 class ExecutorError(Exception):
     pass
@@ -72,28 +103,222 @@ class Executor:
 
         # Do it!
         process = partial(self.process, hidden_states=scenario_state.adapter_spec.hidden_states)
-        request_states = parallel_map(
-            process,
-            scenario_state.request_states,
-            parallelism=self.execution_spec.parallelism,
-        )
-        #request_states = [process(request_state) for request_state in scenario_state.request_states]
+        # request_states = parallel_map(
+        #     process,
+        #     scenario_state.request_states,
+        #     parallelism=self.execution_spec.parallelism,
+        # )
+        #using tqdm
+        if torch.cuda.is_available():
+            #hlog("CUDA is available, initializing with a GPU...")
+            device: str = "cuda"
+        else:
+            device = "cpu"
+        model_kwargs = {}
+        model_kwargs["output_hidden_states"] = True
+        model_kwargs["device_map"]="auto"
+        model_name = "gpt2"
+        model = AutoModelForCausalLM.from_pretrained(model_name, trust_remote_code=True, **model_kwargs)
+        tokenizer = AutoTokenizer.from_pretrained(model_name, **model_kwargs)
+        request_states = [process(model, tokenizer, device,request_state) for request_state in tqdm(scenario_state.request_states, desc="Processing requests")]
 
         hlog(f"Processed {len(request_states)} requests")
         return ScenarioState(scenario_state.adapter_spec, request_states)
 
 
     # horrible to add hidden_geoemtry here but otherwise it require a major refactoring
-    def process(self, state: RequestState, hidden_states = False) -> RequestState:
-        try:
-            result: RequestResult = self.service.make_request(self.execution_spec.auth, state.request, hidden_states=hidden_states)
+    # def process(self, state: RequestState, hidden_states = False) -> RequestState:
+    #     try:
+    #         result: RequestResult = self.service.make_request(self.execution_spec.auth, state.request, hidden_states=hidden_states)
 
-        except Exception as e:
-            raise ExecutorError(f"{str(e)} Request: {state.request}") from e
-        if not result.success:
-            if result.error_flags and not result.error_flags.is_fatal:
-                hlog(f"WARNING: Non-fatal error treated as empty completion: {result.error}")
-                result.completions = [Sequence(text="", logprob=0, tokens=[])]
+    #     except Exception as e:
+    #         raise ExecutorError(f"{str(e)} Request: {state.request}") from e
+    #     if not result.success:
+    #         if result.error_flags and not result.error_flags.is_fatal:
+    #             hlog(f"WARNING: Non-fatal error treated as empty completion: {result.error}")
+    #             result.completions = [Sequence(text="", logprob=0, tokens=[])]
+    #         else:
+    #             raise ExecutorError(f"{str(result.error)} Request: {state.request}")
+    #     return replace(state, result=result)
+    def process(self, model,tokenizer,device, state: RequestState, hidden_states = False ) -> RequestState:
+            try:
+                
+                result: RequestResult = make_request(model,tokenizer,device,state.request, hidden_states=hidden_states)
+
+            except Exception as e:
+                raise ExecutorError(f"{str(e)} Request: {state.request}") from e
+            if not result.success:
+                if result.error_flags and not result.error_flags.is_fatal:
+                    hlog(f"WARNING: Non-fatal error treated as empty completion: {result.error}")
+                    result.completions = [Sequence(text="", logprob=0, tokens=[])]
+                else:
+                    raise ExecutorError(f"{str(result.error)} Request: {state.request}")
+            return replace(state, result=result)
+def make_request(model,tokenizer,device,request: Request, **kwargs) -> RequestResult:
+
+        # Embedding not supported for this model
+        if request.embedding:
+            return EMBEDDING_UNAVAILABLE_REQUEST_RESULT
+
+        # Only a single stop sequence is supported as we can only pass in a single value for `eos_token_id`
+        if len(request.stop_sequences) > 1:
+            raise ValueError("More than one stop sequence is not supported.")
+
+        raw_request = {
+            "engine": request.model_engine,
+            "prompt": request.prompt,
+            "temperature": 1e-7 if request.temperature == 0 else request.temperature,
+            "num_return_sequences": request.num_completions,
+            "max_new_tokens": request.max_tokens,
+            "top_p": request.top_p,
+            "echo_prompt": request.echo_prompt,
+            "top_k_per_token": request.top_k_per_token,
+            "stop_sequences": request.stop_sequences,
+            "output_hidden_states": kwargs.get("hidden_states", False)
+        }
+
+        
+
+
+        response = serve_request(raw_request, model, tokenizer, device)
+        completions = []
+        for raw_completion in response["completions"]:
+            sequence_logprob: float = 0
+            tokens: List[Token] = []
+            if request.echo_prompt:
+                # Add prompt to list of generated tokens.
+                generated_tokens = raw_completion["tokens"][response["input_length"] :]
+                for token_text in raw_completion["tokens"][: response["input_length"]]:
+                    tokens.append(Token(text=token_text, logprob=0.0, top_logprobs={}))
             else:
-                raise ExecutorError(f"{str(result.error)} Request: {state.request}")
-        return replace(state, result=result)
+                generated_tokens = raw_completion["tokens"]
+
+            # Compute logprob for the entire sequence.
+            for token_text, logprob, top_logprobs_dict in zip(
+                generated_tokens, raw_completion["logprobs"], raw_completion["top_logprobs_dicts"]
+            ):
+                tokens.append(Token(text=token_text, logprob=logprob, top_logprobs=top_logprobs_dict))
+                sequence_logprob += logprob
+            
+            #modifying the hidden states so to keep only the last token and the avg of the tokens of the question
+            hidden_states = raw_completion["hidden_states"]
+
+            completion = Sequence(text=raw_completion["text"], logprob=sequence_logprob, tokens=tokens, hidden_states=hidden_states)
+            completion = truncate_sequence(completion, request)
+            completions.append(completion)
+
+        return RequestResult(
+            success=True,
+            cached=False,
+            request_time=0.0,
+            request_datetime=response.get("request_datetime"),
+            completions=completions,
+            embedding=[],
+        )
+        
+def serve_request(raw_request: Dict[str, Any], model, tokenizer, device):
+    #-----------------------------------------
+    encoded_input = tokenizer(raw_request["prompt"], return_tensors="pt", return_token_type_ids=False).to(
+        device
+    )
+
+    raw_request = deepcopy(raw_request)
+    raw_request["do_sample"] = True
+    raw_request["return_dict_in_generate"] = True
+    raw_request["output_scores"] = True
+    top_k_per_token: int = raw_request["top_k_per_token"]
+    del raw_request["top_k_per_token"]
+    #-----------------------------------------
+    # getting index of last question
+    index_in_prompt = raw_request["prompt"].rfind("Question")
+    tokens_question = tokenizer(raw_request["prompt"][index_in_prompt:], return_tensors="pt", return_token_type_ids=False)
+    len_tokens_question = tokens_question["input_ids"].shape[1]
+    #-----------------------------------------
+    
+    if len(raw_request["stop_sequences"]) > 0:
+        stop_sequence_ids = tokenizer(
+            raw_request["stop_sequences"], return_token_type_ids=False, add_special_tokens=False
+        )
+        assert len(stop_sequence_ids.input_ids) == 1, "Total number of stop words should be 1."
+        assert len(stop_sequence_ids.input_ids[0]) == 1, "Total number of tokens in each stop word should be 1."
+        del raw_request["stop_sequences"]
+        raw_request["eos_token_id"] = stop_sequence_ids.input_ids[0][0]
+
+    # Strip out irrelevant parameters
+    relevant_raw_request = {
+        key: raw_request[key]
+        for key in raw_request
+        if key not in ["engine", "prompt", "echo_prompt", "stop_sequences"]
+    }
+
+    # TODO: using GenerationConfig
+    #-----------------------------------------
+    # Use HuggingFace's `generate` method.
+    output = model.generate(**encoded_input, **relevant_raw_request)
+    sequences = output.sequences
+    scores = output.scores
+    
+    #storing hidden states
+    if raw_request["output_hidden_states"]:
+        # instance_hiddenstates = {"len_tokens_question": len_tokens_question, "hidden_states" : torch.cat(output.hidden_states[0])}
+        # hidden_states = hidden_states_process(instance_hiddenstates)
+        #print(f'{"x"*100}\n{hidden_states["sum"].shape=}\n{hidden_states["last"].shape=}\n{"x"*100}')
+        # del instance_hiddenstates
+
+        hs = torch.cat(output.hidden_states[0][-len_tokens_question:]).detach().cpu().numpy()
+        # cropped_hidden_states = [copy.deepcopy(i[-len_tokens_question:,:].mean(0).detach().cpu().numpy()) for i in output.hidden_states[0]]
+        # # for i in output.hidden_states[0]:
+        # #     cropped_hidden_states.append(copy.deepcopy(i[-len_tokens_question:,:].mean(0).detach().cpu().numpy()))
+        # hidden_states = cropped_hidden_states
+        # del output
+        #hidden_states =reduce(copy.deepcopy(hs[:,-len_tokens_question:,:]), "l s d -> l d", "mean")
+        hidden_states = {"last": copy.deepcopy(hs[:,-1,:]), "sum":reduce(copy.deepcopy(hs[:,-len_tokens_question:,:]), "l s d -> l d", "mean")}
+    else:
+        hidden_states = None
+
+    # Compute logprobs for each completed sequence.
+    all_logprobs_of_chosen_tokens = []
+    all_top_logprobs_dicts = []
+    for completion_id in range(raw_request["num_return_sequences"]):
+        logprobs_of_chosen_tokens = []
+        top_logprobs_dicts = []
+        for i in range(len(sequences[completion_id]) - len(encoded_input.input_ids[0])):
+            logprobs = torch.nn.functional.log_softmax(scores[i][completion_id], dim=0)
+
+            # Get top tokens in terms of log probability.
+            topk_logprobs = torch.topk(logprobs, k=top_k_per_token)
+            top_logprobs_dicts.append(
+                {
+                    tokenizer.convert_ids_to_tokens(k.item()): v.item()
+                    for (k, v) in zip(topk_logprobs.indices, topk_logprobs.values)
+                }
+            )
+
+            # Get log probability of chosen token.
+            j = i + len(encoded_input.input_ids[0])
+            logprobs_of_chosen_tokens.append(logprobs[sequences[completion_id][j]].item())
+        all_logprobs_of_chosen_tokens.append(logprobs_of_chosen_tokens)
+        all_top_logprobs_dicts.append(top_logprobs_dicts)
+
+    # Remove prompt from the start of each sequence if echo_prompt is False.
+    if not raw_request["echo_prompt"]:
+        sequences = [sequence[len(encoded_input.input_ids[0]) :] for sequence in sequences]
+
+    all_tokens = [[tokenizer.decode(token) for token in sequence_tokens] for sequence_tokens in sequences]
+    all_decoded_text = tokenizer.batch_decode(sequences)
+
+    completions = []
+    for decoded_text, tokens, logprobs_of_chosen_tokens, top_logprobs_dicts in zip(
+        all_decoded_text, all_tokens, all_logprobs_of_chosen_tokens, all_top_logprobs_dicts
+    ):
+        completions.append(
+            {
+                "text": decoded_text,
+                "tokens": tokens,
+                "logprobs": logprobs_of_chosen_tokens,
+                "top_logprobs_dicts": top_logprobs_dicts,
+                "hidden_states": hidden_states
+            }
+        )
+    torch.cuda.empty_cache()
+    return {"completions": completions, "input_length": len(encoded_input.input_ids[0])}
